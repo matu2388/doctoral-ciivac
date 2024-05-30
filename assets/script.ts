3bb28248b244c8c7de31ad4b951f46a445344434
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 11:24:25 -0300

    llevando desde vsc

--- assets/script.ts
+++ assets/script.ts
@@ -3,12 +3,12 @@ import { tileLayerSelect } from '../../config/tile-layer/functions';
 import { startMapTemplate } from '../../assets/template/content';
 import { tileLayers } from '../../config/tile-layer/data';
 
-startMapTemplate(document, 'Sección 2 - Pop-up con Marcadores y sin Marcadores');
+startMapTemplate(document, '');
 
-const mymap = new Map('map').setView([-26.9866, -66.7381], 13);
+const map = new Map('map').setView([-26.9866, -66.7381], 13);
 
 tileLayerSelect(tileLayers.baseLayers.default.map, {
     attribution: tileLayers.baseLayers.default.atribution
-}).addTo(mymap);
+}).addTo(map);
 
-marker( [-26.988259032403032, -66.73797058911501]).addTo(mymap);
+marker( [-26.988259032403032, -66.73797058911501]).addTo(map);
