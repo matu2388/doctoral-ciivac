baea9354565029fb944ebe18967eabb1287b72a6
matu2388 <87373041+matu2388@users.noreply.github.com>
2024-05-30 11:18:59 -0300

    Create option.interface.ts

---
+++ config/tile-layer/option.interface.ts
@@ -0,0 +1,10 @@
+export interface ITilerLayerOptions {
+ minZoom?: number;
+ maxZoom?: number;
+ errorTileUrl?: any;
+ layers?: string,
+ format?: string,
+ transparent?: boolean,
+ attribution?: any;
+
+}
